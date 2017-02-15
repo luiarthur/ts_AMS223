a599482f85a6fffdeca5c0942744d96e9f28f75a
Arthur Lui <luiarthur@gmail.com>
2017-02-15 11:51:20 -0800

    yes

--- hw/hw3/src/3-3.R
+++ hw/hw3/src/3-3.R
@@ -1,4 +1,4 @@
-dat <- ts(read.table("../dat/soi.dat", skip=16, header=FALSE))
+dat <- ts(read.table("../dat/soi.dat", skip=16, header=FALSE)[,1])
 
 n <- length(dat)
 stopifnot(n == 540 && dat[1] == 2.993 && dat[n] == -1.006)
@@ -14,7 +14,7 @@ acf(dat)
 pacf(dat)
 par(mfrow=c(1,1))
 
-k <- seq(1,floor(n/2-.5),by=1)
+k <- seq(1, floor(n/2 - .5), by=1)
 y <- dat
 
 a.hat <- sapply(k, function(ki) 
@@ -24,7 +24,8 @@ b.hat <- sapply(k, function(ki)
                 2/n * sum( y * sin(2*pi*ki/n * (1:n)) ))
 
 pgram <- (a.hat^2 + b.hat^2) * n / 2
-
+w <- 2*pi*k/n
 plot(2*pi*k/n, pgram, type='o', pch=16)
-#plot(n/k, pgram * 2*pi*k^2/n^2, type='o', pch=16)
-#which.max(pgram * 2*pi*k^2/n^2)
+plot(2*pi*k/n, log(pgram), type='o', pch=16,type='l')
+spec.pgram(dat)
+#plot(n/k, pgram * w*k/n, type='o', pch=16)
--- hw/hw3/src/3-4.R
+++ hw/hw3/src/3-4.R
@@ -1,1 +1,3 @@
 data(lh)
+
+spec.pgram(lh)
