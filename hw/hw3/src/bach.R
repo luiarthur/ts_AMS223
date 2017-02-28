1475e8bbe418f2b8ec4017282fc9fa57f5056a9a
Arthur Lui <luiarthur@gmail.com>
2017-02-27 21:26:17 -0800

    yes

--- hw/hw3/src/bach.R
+++ hw/hw3/src/bach.R
@@ -1,13 +1,5 @@
 library(dlm)
-dlmFilterDF <- function(y,mod,debug=FALSE,simplify=FALSE,delta=.8) {
-  G <- mod$GG
-  P <- G %*% (mod$C0) %*% t(G)
-  W(mod) <- P / delta 
-  dlmFilter(y,mod,debug,simplify)
-}
-
 comp <- read.csv("../dat/bach_beet.csv", skip=5)
-
 col.bach <- "orange"
 
 times <- levels(comp[,1])
@@ -16,28 +8,35 @@ N <- nrow(comp)
 ind <- seq(1,N,by=12)
 total.years <- round(N/12)
 
+
+# DLM:
+nTrain <- 12*8; p <- 12; k <- 2; dV <- 1
+bachMod <- dlmModPoly(k,dV=dV, dW=c( rep(1,k-1), 0)) + 
+           dlmModSeas(p,dV=dV, dW=c( rep(1,p-2), 0))
+
+bachFilt <- dlmFilter(bach[1:nTrain], bachMod)
+bachFuture <- dlmForecast(bachFilt,nAhead=N-nTrain)
+
 plot(0,0, xlim=c(0,N), ylim=c(0,110), type='n',bty='n',fg='grey',xaxt='n',
      xlab='',ylab='Score',las=2)
 axis(1,fg='grey',at=ind,label=sapply(times[ind],function(x)substr(x,1,4)),las=1)
-
 abline(v=seq(1,N,by=6),col='grey80',lty=3)
 abline(v=ind,col='grey80',lty=2)
-lines(bach,type='l',lwd=3,pch=20,cex=.5,col=col.bach)
 
-# DLM:
-nTrain <- 12*11; p <- 12; k <- 2; dV <- 10; delta <- .9
-bachMod <- dlmModPoly(k,dV=dV, dW=c( rep(1,k-1), 1)) + 
-           dlmModSeas(p,dV=dV, dW=c( rep(1,p-2), 1))
+future_ind <- (nTrain+1):N
+lines(1:nTrain, bach[1:nTrain],type='l',lwd=3,pch=20,cex=.5,col=col.bach)
+lines(future_ind, bach[future_ind],type='l',lwd=3,pch=20,cex=.5,col=col.bach,lty=3)
+lines(c(bachFilt$f,bachFuture$f),lty=3,col='grey30',lwd=2)
 
-#bachFilt <- dlmFilterDF(bach[1:nTrain], bachMod, delta=delta)
-bachFilt <- dlmFilter(bach[1:nTrain], bachMod)
-bachFuture <- dlmForecast(bachFilt,nAhead=N-nTrain,sampleNew=10)
+Q <- sapply(bachFuture$Q, function(x) x[[1]])
+lines(future_ind,bachFuture$f[,1] - sqrt(Q) * 1.96, lty=2)
+lines(future_ind,bachFuture$f[,1] + sqrt(Q) * 1.96, lty=2)
 
-plot(bach,type='l',col=col.bach,xlim=c(0,N),ylim=c(0,100),lwd=2)
-lines(c(bachFilt$f,bachFuture$f),lty=2,col='grey30')
-abline(v=nTrain)
-
-bachFuture$newObs[[10]][,1]
-lapply(bachFuture$newObs, function(newobs) lines(newobs[tail(1:nTrain,N-nTrain),1]))
-
+#bachFuture$newObs[[1]][,1]
+#bachFuture <- dlmForecast(bachFilt,nAhead=N-nTrain,sampleNew=1000)
+#pred <- sapply(bachFuture$newObs, function(newobs) newobs[,1])
+#ci <- apply(pred,1,quantile,c(.025,.975))
+#lines(ind, ci[1,])
+#lines(ind, ci[2,])
+#lines(ind, apply(pred,1,mean), col='blue',lwd=2)
 
