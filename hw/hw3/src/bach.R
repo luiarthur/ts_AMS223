e54110be513c338a03234741af598c85bf807163
Arthur Lui <luiarthur@gmail.com>
2017-02-27 21:38:23 -0800

    yes

--- hw/hw3/src/bach.R
+++ hw/hw3/src/bach.R
@@ -1,4 +1,5 @@
 library(dlm)
+library(rcommon)
 comp <- read.csv("../dat/bach_beet.csv", skip=5)
 col.bach <- "orange"
 
@@ -10,9 +11,9 @@ total.years <- round(N/12)
 
 
 # DLM:
-nTrain <- 12*8; p <- 12; k <- 2; dV <- 1
-bachMod <- dlmModPoly(k,dV=dV, dW=c( rep(1,k-1), 0)) + 
-           dlmModSeas(p,dV=dV, dW=c( rep(1,p-2), 0))
+nTrain <- 12*8; p <- 12; k <- 3; dV <- 1
+bachMod <- dlmModPoly(k, dV=dV, dW=c( rep(1,k-1), 0)) + 
+           dlmModSeas(p, dV=dV, dW=c( rep(1,p-2), 0))
 
 bachFilt <- dlmFilter(bach[1:nTrain], bachMod)
 bachFuture <- dlmForecast(bachFilt,nAhead=N-nTrain)
@@ -26,10 +27,8 @@ abline(v=ind,col='grey80',lty=2)
 future_ind <- (nTrain+1):N
 lines(1:nTrain, bach[1:nTrain],type='l',lwd=3,pch=20,cex=.5,col=col.bach)
 lines(future_ind, bach[future_ind],type='l',lwd=3,pch=20,cex=.5,col=col.bach,lty=3)
-lines(c(bachFilt$f,bachFuture$f),lty=3,col='grey30',lwd=2)
+lines(c(bachFilt$f,bachFuture$f),lty=2)
 
-Q <- sapply(bachFuture$Q, function(x) x[[1]])
-lines(future_ind,bachFuture$f[,1] - sqrt(Q) * 1.96, lty=2)
-lines(future_ind,bachFuture$f[,1] + sqrt(Q) * 1.96, lty=2)
-
-
+ci.lower <- bachFuture$f[,1] - sqrt(Q) * 1.96
+ci.upper <- bachFuture$f[,1] + sqrt(Q) * 1.96
+color.btwn(future_ind, ci.upper, ci.lower, from=0, to=1000,col.area=rgb(0,0,0,.1))
