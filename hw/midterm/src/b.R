93b5759cfd510a76907f45144f850e673a7bda44
Arthur Lui <luiarthur@gmail.com>
2017-03-02 23:28:16 -0800

    yes

--- hw/midterm/src/b.R
+++ hw/midterm/src/b.R
@@ -1,3 +1,4 @@
+set.seed(223)
 source("kfilter.R")
 library(rcommon)
 
@@ -10,21 +11,43 @@ total.years <- round(N/12)
 ind <- c(0,seq(12,N,by=12))
 label <- c('2004', sapply(t_axis[ind],function(x)substr(x,1,4)))
 
+# OBSERVED PREDICTIVE LOG DENSITY
+delta_grid_size <- 50
+delta_grid <- seq(.5,.99,len=delta_grid_size)
+lls <- sapply(delta_grid, function(d) {
+  filt <- kfilter(ucsc,delta=d,m0=50,C0=100)
+  ll_pred_density(filt,B=1000)
+})
+lls_mean <- apply(lls,2,mean)
+lls_ci <- apply(lls,2,quantile,c(.025,.975))
 
-filt <- kfilter(ucsc,delta=.8,m0=50,C0=100)
+plot(delta_grid,lls_mean,type='b',col='steelblue',ylim=range(lls_ci),
+     lwd=3,bty='n',fg='grey',ylab='density',xlab=expression(delta),
+     main='Observed Predicted log Density\n\n',col.main='grey30')
+color.btwn(delta_grid, lls_ci[1,],lls_ci[2,],from=0,to=delta_grid_size,
+           col.area=rgb(0,0,0,.2))
+delta.hat <- delta_grid[which.max(lls_mean)]
+abline(v=delta.hat,col='grey',lwd=2)
+title(main=bquote(hat(delta) == .(delta.hat)))
+
+
+# Analysis
+filt <- kfilter(ucsc,delta=delta.hat,m0=50,C0=100) # delta.hat=.87?
 
 theta <- kfilter.theta(filt,1000)
 theta.mean <- apply(theta,2,mean)
 ci <- apply(theta,2,quantile,c(.025,.975))
 
+
 # Plots
-plot(0,0, xlim=c(0,N+20), ylim=c(0,100), type='n',bty='n',
+plot(0,0, xlim=c(0,N+12), ylim=c(0,100), type='n',bty='n',
      fg='grey',xaxt='n',
      xlab='Year',ylab='Google Volume Index',las=2,
      main='Google Trend – UCSC Websearches',col.main='grey30')
 
-axis(1,fg='grey',at=ind,label=label,las=1)
-abline(v=ind,col='grey80',lty=2)
+axis(1,fg='grey',at=c(ind,tail(ind,1)+12),
+     label=c(label,tail(strtoi(label),1)+1),las=1)
+abline(v=c(ind,tail(ind,1)+12),col='grey80',lty=2)
 lines(1:N, ucsc, col='grey30',lwd=1)
 
 lines(1:N,theta.mean,lty=2,col='grey30')
@@ -34,6 +57,8 @@ color.btwn(1:N,ci[1,],ci[2,],from=1,to=N,col.area=rgb(0,0,0,.2))
 y_new <- rep(tail(theta.mean,1),12)
 sd_new <- matrix(ci[,ncol(ci)],nrow=12,ncol=2,byrow=TRUE)
 fut_ind <- (N+1):(N+12)
-lines(fut_ind, y_new, col='steelblue', lwd=3)
+lines(fut_ind, y_new, col='steelblue', lwd=3,type='o')
 color.btwn(fut_ind,sd_new[,1],sd_new[,2],from=N+1,to=N+12,
            col.area=rgb(0,0,1,.2))
+
+
--- hw/midterm/src/kfilter.R
+++ hw/midterm/src/kfilter.R
@@ -1,9 +1,8 @@
 kfilter <- function(y,m0=0,C0=1,n0=1,d0=1,delta=.9) {
   N <- length(y)
 
-
   param <- as.list(1:N)
-
+  prior <- list(m=m0, C=C0, n=n0, d=d0)
 
   update_R <- function(C_prev,W_curr) C_prev + W_curr
   update_Q <- function(R_curr) R_curr + 1
@@ -22,12 +21,8 @@ kfilter <- function(y,m0=0,C0=1,n0=1,d0=1,delta=.9) {
     d_prev + (y_curr-m_prev)^2 / Q_curr
   }
 
-  W1 <- update_W(C0)
-  R1 <- update_R(C0,W1)
-  Q1 <- update_Q(R1)
-  param[[1]] <- list(m=m0, C=C0, n=n0, d=d0, Q=Q1, R=R1)
-  for (i in 2:N) {
-    prev <- param[[i-1]]
+  for (i in 1:N) {
+    prev <- if(i>1) param[[i-1]] else prior
 
     W <- update_W(prev$C)
     R <- update_R(prev$C,W)
@@ -41,23 +36,32 @@ kfilter <- function(y,m0=0,C0=1,n0=1,d0=1,delta=.9) {
     param[[i]] <- list(m=m,C=C,n=n,d=d,Q=Q,R=R)
   }
 
-  list(y=y, delta=delta, param=param)
+  list(y=y, delta=delta, param=param, prior=prior)
 }
 
 kfilter.theta <- function(filt,B=1000) {
-
   samp <- function(param) {
-    oneSamp <- function(dummy) {
-      v <- 1 / rgamma(1, param$n/2, rate=param$d/2)
-      rnorm(1, param$m, sqrt(v*param$C))
-    }
-
-    sapply(1:B, oneSamp)
+    v <- 1 / rgamma(B, param$n/2, rate=param$d/2)
+    rnorm(B, param$m, sqrt(v*param$C))
   }
 
   sapply(filt$param, samp)
 }
 
+
+ll_pred_density <- function(filt,B=1000) {
+  N <- length(filt$y)
+  param <- filt$param
+
+  ll <- sapply(2:N, function(i) {
+    p <- param[[i]]
+    m_prev <- param[[i-1]]$m
+    v <- 1 / rgamma(B, p$n/2, rate=p$d/2)
+    dnorm(filt$y[i], m_prev, sqrt(v*p$Q), log=TRUE)
+  })
+
+  return(apply(ll,1,sum))
+}
 
 #forecast <- function(filt,nAhead=1) {
 #  N <- length(filt$y)
