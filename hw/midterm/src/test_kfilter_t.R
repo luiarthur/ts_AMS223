3c4cf8a1ed847a196b21c1c96a59727487d7f17b
Arthur Lui <luiarthur@gmail.com>
2017-03-03 20:42:29 -0800

    yes

--- hw/midterm/src/test_kfilter_t.R
+++ hw/midterm/src/test_kfilter_t.R
@@ -2,8 +2,9 @@
 source("kfilter_t.R")
 
 library(rcommon)
+last <- function(x) x[length(x)]
 
-usuk <- ts(read.csv("../dat/usuk.dat")[,1]) / 100
+usuk <- c(read.csv("../dat/usuk.dat")[,1]) / 100
 
 plot(usuk,type='o',pch=20)
 filt <- kfilter_t(usuk,m0=0,C0=1,n0=1,d0=.01,delta=.8)
@@ -13,13 +14,13 @@ lines(sapply(filt$param, function(p) p$f), lty=1)
 
 delta_grid_size <- 5
 delta_grid <- seq(.6,1,len=delta_grid_size)
+
 lls <- sapply(delta_grid, function(d) {
-  filt <- kfilter_t(usuk,delta=d,m0=0,C0=1,n0=1,d0=.01)
+  filt <- kfilter_t(usuk,m0=0,C0=1,n0=1,d0=.01,delta=d)
   ll_pred_density_t(filt)
 })
 
-
-llr <- lls - tail(lls,1)
+llr <- lls - last(lls)
 
 plot(delta_grid,llr,type='b',col='steelblue',
      lwd=3,bty='n',fg='grey',
