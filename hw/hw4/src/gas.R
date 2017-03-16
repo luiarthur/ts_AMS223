473531a7b804256349c366b4725c7be96875b937
Arthur Lui <luiarthur@gmail.com>
2017-03-15 22:17:04 -0700

    done with order 2 polynomial (linear trend) with any harmonics

--- hw/hw4/src/gas.R
+++ hw/hw4/src/gas.R
@@ -18,6 +18,6 @@ plot(gas,type='p',xlim=c(1,N+nAhead),ylim=range(c(gas,fc.ci)),pch=20,col='grey30
 lines(sapply(filt$param,function(p) t(filt$FF) %*% p$m),type='l',col='blue',lwd=2)
 color.btwn(1:N,filt.ci[1,],filt.ci[2,],from=1,to=N,col.area=rgb(0,0,1,.2))
 
-lines((N+1):(N+nAhead), fc$f,lty=2,lwd=2,col='dodgerblue')
-color.btwn((N+1):(N+nAhead),fc.ci[1,],fc.ci[2,],from=1,to=N+nAhead,col.area=rgb(0,0,1,.2))
+lines((N+1):(N+nAhead), fc$f,lty=2,lwd=2,col='red')
+color.btwn((N+1):(N+nAhead),fc.ci[1,],fc.ci[2,],from=1,to=N+nAhead,col.area=rgb(1,0,0,.2))
 
